1961f984b04f485fc9411ed4f9b24b495aad5019
Pank1-2 <kiko321pan@gmail.com>
2023-04-24 12:37:46 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -4,11 +4,11 @@ import java.net.URI;
 public class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
-            System.out.println("Missing string.");
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
 
-        int port = args.length;
+        int port = Integer.parseInt(args[0]);
 
         Server.start(port, new Handler());
     }
@@ -25,7 +25,7 @@ class Handler implements URLHandler {
         //     System.out.println("Path: " + url.getPath());
         if (url.getPath().contains("/add")) {
             String[] parameters = url.getQuery().split("=");
-            if (parameters[0].contains("/add-message?s")) {
+            if (parameters[0].contains("=")) {
                 x += parameters[1] + '\n';
                 return String.format(x);
             }
